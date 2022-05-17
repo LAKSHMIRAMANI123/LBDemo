011ed305a1a6c20e47406ff6d7eaadc0cbad4597
ramani <lakshmiramanitupakula@gmail.com>
2022-05-17 11:11:10 +0530

    done

---
+++ LBDemo/src/Alien.java
@@ -0,0 +1,8 @@
+import lombok.Data;
+
+public @Data class Alien {
+	
+	private int age;
+	private String name;
+	private String tech;
+}
---
+++ LBDemo/src/Runner.java
@@ -0,0 +1,13 @@
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Alien a1 = new Alien();
+		a1.setAge(21);
+		a1.setName("ramani");
+		a1.setTech("Jobiak");
+		System.out.println(a1);
+	}
+
+}
